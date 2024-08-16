b9cfcc754c576944e05e32fde0f94aaf54dec4fc
amanrai26 <148765502+amanrai26@users.noreply.github.com>
2024-08-16 12:30:56 +0000

    01react.js

---
+++ passgen/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <div className="bg-black">Hello World</div>
+    </div>
+  );
+}
+
+export default App;
